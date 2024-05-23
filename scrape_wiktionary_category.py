c32823896fb9918a338ce06cf4647fdf8265e01b
andrei-stoica26 <andreistoica@foxmail.com>
2024-05-23 12:36:17 +0800

    Add files via upload

--- scrape_wiktionary_category.py
+++ scrape_wiktionary_category.py
@@ -10,14 +10,15 @@ def get_words_from_page(soup):
     words = [x.get_text() for x in links if x.get('href') and '/wiki/' in x.get('href') and all ([y not in x.get('href') for y in filters])]
     return words
 
-def scrape_wiktionary_category(url, do_save_as_pdf = True):
+def scrape_wiktionary_category(url, folder_name, file_title, do_save_as_pdf = True):
     words = []
     page_number = 1
+    start_url = url
     while True:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         words.extend(get_words_from_page(soup))
-        print(f'Scraped page {page_number}.')
+        print(f'Scraped page {page_number} from {start_url}')
         page_number += 1
         next_page_link = soup.find('a', string = 'next page')
         if not next_page_link:
@@ -25,22 +26,24 @@ def scrape_wiktionary_category(url, do_save_as_pdf = True):
             break 
         url = 'https://en.wiktionary.org' + next_page_link.get('href')
         time.sleep(2)
-    if not os.path.exists('Output'):
-        os.makedirs('Output')
-    with open('Output/words.txt', 'w') as f:
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    with open(f'{folder_name}/{file_title}.txt', 'w') as f:
         f.write('\n'.join(sorted(list(set(words)))))
     if do_save_as_pdf:
         pdf = FPDF()   
         pdf.add_page()
         pdf.set_font('Times', size = 12)
-        with open('Output/words.txt') as f:
+        with open(f'{folder_name}/{file_title}.txt') as f:
             for x in f:
                 pdf.cell(200, 10, txt = x, ln = 1)
-        pdf.output('Output/words.pdf')
+        pdf.output(f'{folder_name}/{file_title}.pdf')
 
 def main():
     url = 'https://en.wiktionary.org/wiki/Category:Galician_terms_with_unknown_etymologies'
-    scrape_wiktionary_category(url, True)
+    folder_name = 'Output'
+    file_title = 'words'
+    scrape_wiktionary_category(url, folder_name, file_title, True)
 
 if __name__ == '__main__':
     main()
